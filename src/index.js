a641f5f47f13cbc80dbc9ef45673a1bac6870ec2
RobsonMlps <105452402+RobsonMlps@users.noreply.github.com>
2025-07-03 11:40:15 -0300

    mudanças de imagem, titulo, descrição e preço no terceiro card

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 let itens = [{
     src:"assets/imagem/img1.jpg",
-    titulo:"Card title",
-    discricao:"Some quick example text to build on the card title and make up the bulk of the card’s content",
-    preco:"R$ 20",
+    titulo:"Café expresso",
+    discricao:"feito com os melhores grãos de café",
+    preco:"R$ 10",
 }, {
     src:"assets/imagem/embalagem1.avif",
     titulo:"café alto da serra",
     discricao:"café alto da serra",
     preco:"R$ 50",
 }, {
-    src:"assets/imagem/embalagem2.avif",
-    titulo:"Café exrpesso",
-    discricao:"Café expresso istreito",
-    preco:"R$ 10",
+    src:"assets/imagem/bolocaseiro.jpg",
+    titulo:"Bolo da vovó",
+    discricao:"Bolo da vovó feito com carinho e amor",
+    preco:"R$ 5",
 }]
 
 let getCartao = (item) => {
     return `<div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="${item.src}" width="50%" height="30%" class="card-img-top" alt="...">
+            <div class="card" style="width: 20rem;">
+                <img src="${item.src}" width="400px" height="250px" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${item.titulo}</h5>
                 <p class="card-text">${item.discricao}</p>
